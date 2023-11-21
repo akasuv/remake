fa1c11b74bf47f3ad47535af5c1de2f2ef4d1e4d
Wei Su <me@weisu.io>
2023-11-21 15:28:13 +0800

    add initial pages

---
+++ app/interview-mentor/page.tsx
@@ -0,0 +1,5 @@
+const InterviewMentorPage = () => {
+  return <h1>Interview Mentor</h1>;
+};
+
+export default InterviewMentorPage;
---
+++ app/resume-writer/page.tsx
@@ -0,0 +1,5 @@
+const ResumeWriterPage = () => {
+  return <h1>Resume Writer</h1>;
+};
+
+export default ResumeWriterPage;
